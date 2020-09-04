76f47caf6be7a6d5cafff2b2d47e069fec846ca4
ajhayden <55779059+ajhayden@users.noreply.github.com>
2020-09-04 12:32:52 -0600

    HW 1 app

---
+++ HW1/Shared/HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW1App.swift
+//  Shared
+//
+//  Created by Student on 9/4/20.
+//
+
+import SwiftUI
+
+@main
+struct HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
