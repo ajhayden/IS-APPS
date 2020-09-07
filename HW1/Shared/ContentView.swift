daf169542e7d557171f27a2bc1eb8e279e13b795
ajhayden <55779059+ajhayden@users.noreply.github.com>
2020-09-07 00:13:42 -0600

    Working functions for hw1

--- HW1/Shared/ContentView.swift
+++ HW1/Shared/ContentView.swift
@@ -8,10 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var fib_num: String = "1"
+    @State var fac_num: String = "1"
+    @State var sum_num1: String = "1"
+    @State var sum_num2: String = "2"
+    @State var coin_num: String = "1"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("HW 1").padding()
+           
+            Group {
+                Text("Fibonacci sequence")
+                TextField("Fibonacci sequence", text: $fib_num)
+                Text(compute_fibonacci(fib_num_amount: Int(fib_num) ?? 0))
+            }
+            
+            Group {
+                Text("Factorial number")
+                TextField("Factorial number", text: $fac_num)
+                Text(compute_factorial(fac_num_amount: Int(fac_num) ?? 0))
+            }
+
+            Group {
+                Text("Sum of all integers between two numbers")
+                TextField("Number 1", text: $sum_num1)
+                TextField("Number 2", text: $sum_num2)
+                Text(compute_sum_of_all_integers(first_num: Int(sum_num1) ?? 0, second_num: Int(sum_num2) ?? 0))
+            }
+
+            Group {
+                Text("Least amount of coins")
+                TextField("Fibonacci sequence", text: $coin_num)
+                Text(compute_coins_for_amount(amount: Int(coin_num) ?? 0))
+            }
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +52,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ HW1/Shared/source_functions.swift
@@ -0,0 +1,136 @@
+//
+//  source_functions.swift
+//  HW1
+//
+//  Created by Student on 9/6/20.
+//
+
+import Foundation
+
+// Calculate the fibonacci sequence number
+func compute_fibonacci(fib_num_amount: Int) -> String {
+    guard fib_num_amount > 1 else {return String(fib_num_amount)}
+    var first_num = 0
+    var second_num = 1
+    
+    for _ in 1...fib_num_amount {
+        let temp = second_num
+        second_num = first_num + second_num
+        first_num = temp
+    }
+    
+    return String(second_num)
+}
+
+// Calculate the factorial of a given number >= 0
+func compute_factorial(fac_num_amount: Int) -> String {
+    var num_array = [Int]()
+    var output = 1
+    for n in 1...fac_num_amount {
+        num_array.append(n)
+    }
+    for n in num_array {
+        output *= n
+    }
+    return String(output)
+}
+
+func compute_sum_of_all_integers(first_num: Int, second_num: Int) -> String {
+    var sum = 0
+    for n in first_num...second_num {
+        sum += n
+    }
+    return String(sum)
+}
+
+func compute_coins_for_amount(amount: Int) -> String {
+    var total_quarters = 0
+    var total_dimes = 0
+    var total_nickels = 0
+    var total_pennies = 0
+    
+    var quarter_str = "quarters"
+    var dime_str = "dimes"
+    var nickel_str = "nickels"
+    var penny_str = "pennies"
+
+    var current_total = amount
+    
+    while current_total >= 25 {
+        current_total -= 25
+        total_quarters += 1
+    }
+
+    while current_total >= 10 {
+        current_total -= 10
+        total_dimes += 1
+    }
+
+    while current_total >= 5 {
+        current_total -= 5
+        total_nickels += 1
+    }
+
+    while current_total >= 1 {
+        current_total -= 1
+        total_pennies += 1
+    }
+    
+    var total_quarters_str = String(total_quarters)
+    var total_dimes_str = String(total_dimes)
+    var total_nickels_str = String(total_nickels)
+    var total_pennies_str = String(total_pennies)
+    
+    if(total_quarters == 1) {
+        quarter_str = "quarter"
+    }
+    
+    if(total_quarters == 0) {
+        quarter_str = ""
+        total_quarters_str = ""
+    }
+    
+    if(total_dimes == 1) {
+        dime_str = "dime"
+    }
+    
+    if(total_dimes == 0) {
+        dime_str = ""
+        total_dimes_str = ""
+    }
+
+    if(total_nickels == 1) {
+        nickel_str = "nickel"
+    }
+    
+    if(total_nickels == 0) {
+        nickel_str = ""
+        total_nickels_str = ""
+    }
+
+    if(total_pennies == 1) {
+        penny_str = "penny"
+    }
+    
+    if(total_pennies == 0) {
+        penny_str = ""
+        total_pennies_str = ""
+    }
+    
+    var output = ""
+    
+    if(total_quarters >= 1) {
+        output = "\(total_quarters_str) \(quarter_str)"
+    }
+    if(total_dimes >= 1) {
+        output += "\n\(total_dimes_str) \(dime_str)"
+    }
+    if(total_nickels >= 1) {
+        output += "\n\(total_nickels_str) \(nickel_str)"
+    }
+    if(total_pennies >= 1) {
+        output += "\n\(total_pennies_str) \(penny_str)"
+    }
+
+    return output
+}
